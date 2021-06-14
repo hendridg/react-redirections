9654e7db1067d117e1a1508c9326cc07b7b233d4
hendridg <73659934+hendridg@users.noreply.github.com>
2021-06-14 16:48:29 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import { FirebaseContext } from "../src/context/firebase"
 import store from "./redux"
 import { Provider } from "react-redux"
 
+//Push you config here
 var firebaseConfig = {
-  apiKey: "AIzaSyAOC7KILB4s7MdcpPIf4-bjX7C06SGqlWE",
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
   authDomain: "material-ui-8e969.firebaseapp.com",
   projectId: "material-ui-8e969",
   storageBucket: "material-ui-8e969.appspot.com",
   messagingSenderId: "1093003317072",
-  appId: "1:1093003317072:web:cf0c1ecbf609f621632655",
+  appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
   measurementId: "G-CNRDZLRHE3",
 }
 // Initialize Firebase
